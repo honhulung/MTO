ec9002eba91d8310b2eb6bccf82b72ac25369637
Ho Nhu Lung <11130072@st.hcmuaf.edu.vn>
2015-05-23 12:40:23 +0700

    Xem nhe cai nay la tab do may dua

---
+++ app/src/main/java/com/example/sv11130072/mto/HomeMain.java
@@ -0,0 +1,23 @@
+package com.example.sv11130072.mto;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class HomeMain extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home_main, container, false);
+    }
+
+
+}
